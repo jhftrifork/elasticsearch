82c965c24815595a832b217d96568e3b9e932b2a
Lukasz Guminski <lguminski@gmail.com>
2015-07-28 18:20:47 +0200

    flow defined

--- flow.groovy
+++ flow.groovy
@@ -1,8 +1,17 @@
-{ ->
-  node('special-slave') {
-    hello 'world'
-  }
-}
-def hello(whom) {
-  echo "hello ${whom}"
+node { 
+    git 'https://github.com/mesos/elasticsearch.git'
+    stage 'Compile'
+
+    //step([$class: 'SlackNotifier'])
+    sh './gradlew clean compileJava'
+
+    stage 'QA'
+    sh './gradlew build jacocoTestReport sonarRunner'
+    step([$class: 'JUnitResultArchiver', testResults: '**/test-results/*xml'])
+
+    sh './gradlew systemTest'
+    step([$class: 'JUnitResultArchiver', testResults: '**/test-results/*xml'])
+
+    stage 'Release'
+    sh './gradlew publishDockerImageWithLatest'
 }
