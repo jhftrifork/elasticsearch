078aa22d99108eab6a3fdb3fcb157913431086f5
James Fisher <jhf@trifork.com>
2015-08-18 09:51:59 +0100

    test that Mesos reports two frameworks after registering two

--- system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
+++ system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
@@ -1,11 +1,16 @@
 package org.apache.mesos.elasticsearch.systemtest;
 
 import org.apache.log4j.Logger;
+import org.apache.mesos.mini.state.Framework;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
+import static com.jayway.awaitility.Awaitility.await;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -14,6 +19,19 @@ import static org.junit.Assert.assertTrue;
 public class DiscoverySystemTest extends TestBase {
 
     public static final Logger LOGGER = Logger.getLogger(DiscoverySystemTest.class);
+
+    @Test
+    public void testSchedulerRegistration() {
+        await().atMost(60, TimeUnit.SECONDS).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                ArrayList<Framework> frameworks = CLUSTER.getStateInfo().getFrameworks();
+                LOGGER.info("Waiting for two frameworks in cluster; got: " + frameworks.toString());
+                frameworks.stream().anyMatch((Framework f) -> f.getName() == getScheduler1().getName());
+                return frameworks.size() == 2;
+            }
+        });
+    }
 
     @Test
     public void testNodeDiscoveryRest() throws InterruptedException {
